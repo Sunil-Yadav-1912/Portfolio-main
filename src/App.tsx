864ecab13ae1d6d06590f6abff326c9163b647bb
Sunil-Yadav-1912 <yadavsunilkumar998@gmail.com>
2026-08-19 12:44:40 +0530

    Test commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ const MyWorks = lazy(() => import("./pages/MyWorks"));
 const Play = lazy(() => import("./pages/Play"));
 import { LoadingProvider } from "./context/LoadingProvider";
 
+//test commit
 const App = () => {
   return (
     <BrowserRouter>
